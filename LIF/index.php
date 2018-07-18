fb5f5e776f424b01bb62cc0666c7aa3592847077
wans <alexwans@gmail.com>
2018-07-18 11:46:24 +0300

    CMSIF in Vendors

--- LIF/index.php
+++ LIF/index.php
@@ -1,6 +1,22 @@
 <?php
 //define('CMSIF_ASSETS', '/lif/'); //if your project live in subdirectory
-require __DIR__.'/cmsif.php';
+
+define('CMSIF_WEBROOT', __DIR__);
+define('CMSIF_COOKIE_LTIME', 3600);
+define('CMSIF_TIMEZONE', 'Europe/Moscow');
+define('CMSIF_DEFAULT_LANG', 'en');
+
+define('CMSIF_FILES', __DIR__.'/__FILES__/');
+define('CMSIF_MODULES', __DIR__.'/__MODULES__/');
+define('CMSIF_TEMPLATES', __DIR__.'/__TEMPLATES__/');
+
+//DB
+define('CMSIF_DB_HOST', 'localhost');
+define('CMSIF_DB_USER', 'root');
+define('CMSIF_DB_PASS', 'toor');
+define('CMSIF_DB_NAME', 'app');
+
+require __DIR__.'/vendors/cmsif/cmsif.php';
 
 init();
 
